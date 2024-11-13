72cd791a4c523b2030d03cac57c202b2ff87125e
Blackwhite43 <willy.liu.cia.chen@gmail.com>
2024-11-13 14:23:12 +0700

    Set SentenceTransformers to 3.1.1

--- Pytorch-CUDA_Install.sh
+++ Pytorch-CUDA_Install.sh
@@ -42,8 +42,7 @@ pip install scikit-learn
 pip install matplotlib
 pip install openpyxl
 pip install datasets
-pip install sentence-transformers
-pip install transformers[torch]==4.45.2
+pip install sentence-transformers==3.1.1
 pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu124
 
 # Deactivate the environment (optional)
